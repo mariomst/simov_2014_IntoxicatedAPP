62cb06d634bd345c9e335b1f7c563ea0fa7c8c8e
Mário Teixeira <1090626@isep.ipp.pt>
2014-11-21 16:31:20 +0000

    Criação do ecrã principal

--- src/pt/isep/intoxicatedapp/MainActivity.java
+++ src/pt/isep/intoxicatedapp/MainActivity.java
@@ -2,8 +2,6 @@ package pt.isep.intoxicatedapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
