1007284144097d483cec5dab450faacee30f6262
Mário Manuel de Sousa Teixeira <1090626@isep.ipp.pt>
2014-12-10 22:20:52 +0000

    Adicionado activity EditContact para edição de contactos existentes na base de dados.

---
+++ src/pt/isep/intoxicatedapp/EditContactActivity.java
@@ -0,0 +1,56 @@
+package pt.isep.intoxicatedapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EditContactActivity extends Activity {
+	
+	protected EditText contactName;
+	protected EditText contactNumber;
+	protected int _id;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_editcontact);
+		
+		/**
+		 * Obter informações do contacto através do seu ID
+		 */
+		DBAdapter adapter = new DBAdapter(getApplicationContext());
+		_id = getIntent().getIntExtra("contactID", 0);
+		Contact c = adapter.getContact(_id);				
+		
+		/**
+		 * Variáveis vão obter as informações inseridas nas respectivas caixas
+		 * de texto
+		 **/
+		contactName = (EditText) findViewById(R.id.contactName);
+		contactNumber = (EditText) findViewById(R.id.contactNumber);
+		
+		/**
+		 * Definir texto nas caixas de texto
+		 */
+		contactName.setText(c.getName());
+		contactNumber.setText(c.getPhoneNumber());
+		
+		/**
+		 * Intruções para o botão Edit
+		 */
+		Button buttonEDIT = (Button) findViewById(R.id.editButton);
+		buttonEDIT.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+            	/** Criar novo objecto Contact **/
+            	Contact c = new Contact(_id, contactName.getText().toString(), contactNumber.getText().toString());
+            	/** Atualizar BD **/
+                DBAdapter adapter = new DBAdapter(getApplicationContext());
+                adapter.updateContact(c.getID(), c.getName(), c.getPhoneNumber());
+                finish();
+            }
+        });
+	}
+}
